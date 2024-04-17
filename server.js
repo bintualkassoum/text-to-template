59826114086c2aeea8312ca75af437b652eedf79
Bintu <bintualkassoum@gmail.com>
2024-04-16 20:36:06 -0400

    Add supabase import and fix supabase require statement. Added error handling.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
+const supabase = require('./supabaseClient');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -16,4 +15,9 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+}); // Add a closing parenthesis here
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
